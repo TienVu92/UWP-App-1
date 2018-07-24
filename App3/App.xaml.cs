e10fad12d8ebfaa320de4f0950e33011ed334465
EXI-Nguyen <vu.t.nguyen6@boeing.com>
2018-07-24 10:24:54 -0700

    DefectBranch12345

--- App3/App.xaml.cs
+++ App3/App.xaml.cs
@@ -32,6 +32,7 @@ namespace App3
             this.Suspending += OnSuspending;
         }
         public string TestString;
+        public string BranchDefect12345;
         /// <summary>
         /// Invoked when the application is launched normally by the end user.  Other entry points
         /// will be used such as when the application is launched to open a specific file.
